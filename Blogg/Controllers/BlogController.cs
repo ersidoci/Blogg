989879ce4e89d4c65a2d55ec4ec788f05e5cb14e
user <user@DESKTOP-N1BPRMT>
2022-09-22 22:57:39 +0200

    Add project files.

---
+++ Blogg/Controllers/BlogController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Blogg.Models;
+using System.Linq;
+
+namespace Blogg.Controllers
+{
+    public class BlogController : Controller
+    {
+        static List<BlogEntry> Posts = new List<BlogEntry>();
+        public IActionResult Index()
+        {
+            return View("Index",Posts);
+        }
+        public IActionResult CreatorPage(Guid id)
+        {
+            if(id != Guid.Empty)
+            {
+                BlogEntry existingEntry = Posts.FirstOrDefault(x => x.Id == id);
+                return View(model: existingEntry);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreatorPage(BlogEntry entry)
+        {
+            if(entry.Id == Guid.Empty)
+            {
+                BlogEntry newEntry = new BlogEntry();
+                newEntry.Content = entry.Content;
+                newEntry.Id = Guid.NewGuid();
+                Posts.Add(newEntry);
+            }
+            else
+            {
+                BlogEntry existingEntry = Posts.FirstOrDefault(x => x.Id == entry.Id);
+                existingEntry.Content = entry.Content;
+            }
+            
+
+            
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Blogg/Models/BlogEntry.cs
@@ -0,0 +1,8 @@
+﻿namespace Blogg.Models
+{
+    public class BlogEntry
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+    }
+}
